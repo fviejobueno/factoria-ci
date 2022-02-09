8404d38177e8835a2181f849386c59c906ecf1b8
Jing Lin <selinajinglin09@gmail.com>
2022-02-09 14:25:48 +0100

    Update return messages

--- src/test/java/com/example/springboot/HelloControllerIT.java
+++ src/test/java/com/example/springboot/HelloControllerIT.java
@@ -18,6 +18,6 @@ public class HelloControllerIT {
     @Test
     public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity("/", String.class);
-        assertThat(response.getBody()).isEqualTo("Welcome to the CI demo session and greetings from Thoughtworks!");
+        assertThat(response.getBody()).isEqualTo("Greetings from Thoughtworks!");
     }
 }
